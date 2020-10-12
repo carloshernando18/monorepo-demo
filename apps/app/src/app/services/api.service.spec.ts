b1e8de94e79763347779f86533077be9f06bfbeb
Carlos <carloshernando18@gmail.com>
2020-10-12 10:28:14 -0500

    test: add test for api service

--- apps/app/src/app/services/api.service.spec.ts
+++ apps/app/src/app/services/api.service.spec.ts
@@ -3,7 +3,7 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
@@ -23,17 +23,17 @@ describe('ApiService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should call getApiMessage', async () => {
-    const data = { message: 'Success' };
-    service.getApiMessage().subscribe((response) => {
-      console.log(response);
-      console.log(expect(response).toStrictEqual('lksd'));
-      expect(response).toBeFalsy();
-    });
-    const request = testingController.expectOne('http://localhost:3333/api');
-    // expect(request.request.method).toBe('GET');
-
-    request.flush(data);
-    testingController.verify();
-  });
+  it(
+    'should call getApiMessage',
+    waitForAsync(() => {
+      const data = { message: 'Success' };
+      service.getApiMessage().subscribe((response) => {
+        expect(response).toStrictEqual('Success');
+      });
+      const request = testingController.expectOne('http://localhost:3333/api');
+      expect(request.request.method).toBe('GET');
+      request.flush(data);
+      testingController.verify();
+    })
+  );
 });
