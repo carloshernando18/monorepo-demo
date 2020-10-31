759cb4bb2dbb680df66ac8e723a1c7d9920d6425
Carlos <carloshernando18@gmail.com>
2020-10-30 21:29:25 -0500

    feat: add MatToolbarÂ

--- libs/app/material/src/lib/app-material.module.ts
+++ libs/app/material/src/lib/app-material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModules = [
   FlexLayoutModule,
@@ -12,6 +13,7 @@ const materialModules = [
   MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
